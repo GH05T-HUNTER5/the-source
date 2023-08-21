bd8be2ec9d5176f3ecc0859b48687e059d01c28c
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-08-21 19:35:58 +0530

    Update color.sh

--- color/color.sh
+++ color/color.sh
@@ -5,17 +5,15 @@ if [[ -d /usr/bin ]]; then
 		echo "Root Must"
 		exit
 	fi
-	git clone https://github.com/Tony-Linux/color
-	cd color
-	mv color /usr/bin/
-	chmod +x /usr/bin/color
-	clear
+	git clone https://github.com/Tony-Linux/color >/dev/null 2>&1
+ 	mv color/color /usr/bin/ >/dev/null 2>&1
+  	chmod +x /usr/bin/color >/dev/null 2>&1
+   	rm -rf color >/dev/null 2>&1
 	color
 elif [[ -d /data/data/com.termux/files/usr/bin ]]; then
-	git clone https://github.com/Tony-Linux/color
-	cd color
-	mv color /data/data/com.termux/files/usr/bin/
-	chmod +x /data/data/com.termux/files/usr/bin/color
-	clear
+	git clone https://github.com/Tony-Linux/color >/dev/null 2>&1	
+	mv color /data/data/com.termux/files/usr/bin/ >/dev/null 2>&1	
+	chmod +x /data/data/com.termux/files/usr/bin/color >/dev/null 2>&1	
+	rm -rf color >/dev/null 2>&1
 	color
 fi
