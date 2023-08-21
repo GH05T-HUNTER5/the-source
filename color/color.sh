1f5a9731d93c7e2e477340e04f3c0385c72fb74b
GH05T HUNTER5 <108191615+GH05T-HUNTER5@users.noreply.github.com>
2023-08-21 19:41:17 +0530

    Update color.sh

--- color/color.sh
+++ color/color.sh
@@ -9,11 +9,13 @@ if [[ -d /usr/bin ]]; then
  	mv color/color /usr/bin/ >/dev/null 2>&1
   	chmod +x /usr/bin/color >/dev/null 2>&1
    	rm -rf color >/dev/null 2>&1
+	rm color.sh >/dev/null 2>&1
 	color
 elif [[ -d /data/data/com.termux/files/usr/bin ]]; then
 	git clone https://github.com/Tony-Linux/color >/dev/null 2>&1	
 	mv color /data/data/com.termux/files/usr/bin/ >/dev/null 2>&1	
 	chmod +x /data/data/com.termux/files/usr/bin/color >/dev/null 2>&1	
 	rm -rf color >/dev/null 2>&1
+ 	rm color.sh >/dev/null 2>&1
 	color
 fi
